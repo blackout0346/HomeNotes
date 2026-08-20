9e33c74a96b5957a0d21a97adc9ac593b238a629
blackout0346 <149482684+blackout0346@users.noreply.github.com>
2026-08-20 21:39:08 +0800

    add attachmentController

--- src/HomeNotes.Api.Server/Controllers/AttachmentController.cs
+++ src/HomeNotes.Api.Server/Controllers/AttachmentController.cs
@@ -1,4 +1,4 @@
-using HomeNotes.Core.DTOs.Notes;
+using HomeNotes.Core.DTOs.Attachments;
 using HomeNotes.Core.Exceptions;
 using HomeNotes.Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -15,6 +15,20 @@ namespace HomeNotes.Api.Server.Controllers
         public AttachmentController(IAttachmentsService attachmentsService) 
         {
             _attachmentService = attachmentsService;
+        }
+        [HttpGet("attachment/{attachmentId}")]
+        [ProducesResponseType(typeof(AttachmentsResponse), 200)]
+        public async Task<IActionResult> GetAttachment([FromRoute] Guid attachmentId)
+        {
+            try
+            {
+                var result = await _attachmentService.GetAttachAsync(attachmentId);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
         }
     }
 }
