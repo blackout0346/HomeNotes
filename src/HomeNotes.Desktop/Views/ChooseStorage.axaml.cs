a7c31e5a17bbe53aaf8b2248ba6de5854f202c04
122 <111@gmail.com>
2026-09-22 11:39:02 +0800

    Add ChooseStorage

---
+++ src/HomeNotes.Desktop/Views/ChooseStorage.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HomeNotes.Desktop.Views;
+
+public partial class ChooseStorage : UserControl
+{
+    public ChooseStorage()
+    {
+        InitializeComponent();
+    }
+}
